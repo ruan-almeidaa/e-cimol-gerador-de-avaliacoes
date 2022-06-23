ee56426914cd30f7293fdd4719c901c1b0e31b68
Ruan <ruanalmeidawegner@gmail.com>
2022-06-23 19:34:37 -0300

    Google Auth

--- functions/auth.js
+++ functions/auth.js
@@ -2,8 +2,8 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
 
 passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  clientID: process.env.GOOGLE_CLIENT_ID ? process.env.GOOGLE_CLIENT_ID : GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET ? process.env.GOOGLE_CLIENT_SECRET : GOOGLE_CLIENT_SECRET,
   callbackURL: "http://localhost:8080/logar/callback",
   passReqToCallback: true,
 },
